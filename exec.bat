32db4f59df57ce8409468960a0e2ee5c0684e073
Felipe Reis <falreis@gmail.com>
2025-07-15 20:06:09 -0300

    Adição de logs.

---
+++ exec.bat
@@ -0,0 +1,69 @@
+python run.py activity_selector
+sleep 30
+python run.py articulation_points
+sleep 30
+python run.py bellman_ford
+sleep 30
+python run.py bfs
+sleep 30
+python run.py binary_search
+sleep 30
+
+
+python run.py bridges
+sleep 30
+python run.py bubble_sort
+sleep 30
+python run.py dag_shortest_paths
+sleep 30
+python run.py dfs
+sleep 30
+python run.py dijkstra
+sleep 30
+
+
+python run.py find_maximum_subarray_kadane
+sleep 30
+python run.py floyd_warshall
+sleep 30
+python run.py graham_scan
+sleep 30
+python run.py heapsort
+sleep 30
+python run.py insertion_sort
+sleep 30
+
+
+python run.py jarvis_march
+sleep 30
+python run.py kmp_matcher
+sleep 30
+python run.py lcs_length
+sleep 30
+python run.py matrix_chain_order
+sleep 30
+python run.py minimum
+sleep 30
+
+
+python run.py mst_kruskal
+sleep 30
+python run.py mst_prim
+sleep 30
+python run.py naive_string_matcher
+sleep 30
+python run.py optimal_bst
+sleep 30
+python run.py quickselect
+sleep 30
+
+
+python run.py quicksort
+sleep 30
+python run.py segments_intersect
+sleep 30
+python run.py strongly_connected_components
+sleep 30
+python run.py task_scheduling
+sleep 30
+python run.py topological_sort

24aa7094981b7456b95aa597d50988c8b4f60240
Felipe Reis <falreis@gmail.com>
2025-08-26 06:39:59 -0300

    Atualização de código e inclusão de logs.

---
+++ exec_hard.bat
@@ -0,0 +1,15 @@
+python run.py activity_selector
+sleep 30
+python run.py bubble_sort
+sleep 30
+python run.py dfs
+sleep 30
+python run.py floyd_warshall
+sleep 30
+python run.py kmp_matcher
+sleep 30
+python run.py naive_string_matcher
+sleep 30
+python run.py quickselect
+sleep 30
+python run.py strongly_connected_components

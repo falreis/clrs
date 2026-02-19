9967ef9b3fff75b2ea65b7301276e5ee52906f5f
Felipe Reis <falreis@gmail.com>
2026-02-19 19:48:17 -0300

    Atualização de épocas.

--- run.py
+++ run.py
@@ -140,7 +140,7 @@ flags.DEFINE_boolean('chunked_training', True,
 flags.DEFINE_integer('chunk_length', 16,
                      'Time chunk length used for training (if '
                      '`chunked_training` is True.')
-flags.DEFINE_integer('train_steps', 12000 + 1, 'Number of training iterations.')
+flags.DEFINE_integer('train_steps', 16000 + 1, 'Number of training iterations.')
 flags.DEFINE_integer('eval_every', 50, 'Evaluation frequency (in steps).')
 flags.DEFINE_integer('test_every', 500, 'Evaluation frequency (in steps).')
 

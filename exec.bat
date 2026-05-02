4a32f73fcb2778cccd4360cfa967c1977054282b
Felipe Reis <falreis@gmail.com>
2026-05-02 17:05:29 -0300

    Atualização de bat de execução.

--- exec.bat
+++ exec.bat
@@ -1,72 +1,72 @@
-python run.py 20000 activity_selector
+python run.py 10000 activity_selector
 sleep 30
-python run.py 20000 articulation_points
+python run.py 10000 articulation_points
 sleep 30
-python run.py 20000 bellman_ford
+python run.py 10000 bellman_ford
 sleep 30
-python run.py 20000 bfs
+python run.py 10000 bfs
 sleep 30
-python run.py 20000 binary_search
-sleep 30
-
-
-python run.py 20000 bridges
-sleep 30
-python run.py 20000 bubble_sort
-sleep 30
-python run.py 20000 dag_shortest_paths
-sleep 30
-python run.py 20000 dfs
-sleep 30
-python run.py 20000 dijkstra
+python run.py 10000 binary_search
 sleep 30
 
 
-python run.py 20000 find_maximum_subarray_kadane
+python run.py 10000 bridges
 sleep 30
-python run.py 20000 floyd_warshall
+python run.py 10000 bubble_sort
 sleep 30
-python run.py 20000 graham_scan
+python run.py 10000 dag_shortest_paths
 sleep 30
-python run.py 20000 heapsort
+python run.py 10000 dfs
 sleep 30
-python run.py 20000 insertion_sort
-sleep 30
-
-
-python run.py 20000 jarvis_march
-sleep 30
-python run.py 20000 kmp_matcher
-sleep 30
-python run.py 20000 lcs_length
-sleep 30
-python run.py 20000 matrix_chain_order
-sleep 30
-python run.py 20000 minimum
+python run.py 10000 dijkstra
 sleep 30
 
 
-python run.py 20000 mst_kruskal
+python run.py 10000 find_maximum_subarray_kadane
 sleep 30
-python run.py 20000 mst_prim
+python run.py 10000 floyd_warshall
 sleep 30
-python run.py 20000 naive_string_matcher
+python run.py 10000 graham_scan
 sleep 30
-python run.py 20000 optimal_bst
+python run.py 10000 heapsort
 sleep 30
-python run.py 20000 quickselect
+python run.py 10000 insertion_sort
 sleep 30
 
 
-python run.py 20000 quicksort
+python run.py 10000 jarvis_march
 sleep 30
-python run.py 20000 segments_intersect
+python run.py 10000 kmp_matcher
 sleep 30
-python run.py 20000 strongly_connected_components
+python run.py 10000 lcs_length
 sleep 30
-python run.py 20000 task_scheduling
+python run.py 10000 matrix_chain_order
 sleep 30
-python run.py 20000 topological_sort
+python run.py 10000 minimum
+sleep 30
+
+
+python run.py 10000 mst_kruskal
+sleep 30
+python run.py 10000 mst_prim
+sleep 30
+python run.py 10000 naive_string_matcher
+sleep 30
+python run.py 10000 optimal_bst
+sleep 30
+python run.py 10000 quickselect
+sleep 30
+
+
+python run.py 10000 quicksort
+sleep 30
+python run.py 10000 segments_intersect
+sleep 30
+python run.py 10000 strongly_connected_components
+sleep 30
+python run.py 10000 task_scheduling
+sleep 30
+python run.py 10000 topological_sort
 sleep 30
 
 shutdown now 
